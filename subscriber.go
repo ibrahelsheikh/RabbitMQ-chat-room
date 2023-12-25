7a84d8cc943b39c5f721492a5c353f0286b46d8d
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2023-12-25 16:54:45 +0200

    Update subscriber.go

--- subscriber.go
+++ subscriber.go
@@ -1,1 +1,84 @@
+/*
+USAGE
+go run receive_logs_topic.go *.info # receives from anything having info in the end
+go run receive_logs_topic.go anonymous.*
+go run receive_logs_topic.go kernel.error
+*/
+package main
 
+import (
+	"load_balancer/utils"
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	utils.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"logs_topic", // name
+		"topic",      // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	utils.FailOnError(err, "Failed to declare an exchange")
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	utils.FailOnError(err, "Failed to declare a queue")
+
+	if len(os.Args) < 2 {
+		log.Printf("Usage: %s [binding_key]...", os.Args[0])
+		os.Exit(0)
+	}
+	for _, binding := range os.Args[1:] {
+		log.Printf("Binding queue %s to exchange %s with binding key %s",
+			q.Name, "logs_topic", binding)
+		err = ch.QueueBind(
+			q.Name,       // queue name
+			binding,      // binding key
+			"logs_topic", // exchange
+			false,
+			nil)
+		utils.FailOnError(err, "Failed to bind a queue")
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto ack
+		false,  // exclusive
+		false,  // no local
+		false,  // no wait
+		nil,    // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
+}
